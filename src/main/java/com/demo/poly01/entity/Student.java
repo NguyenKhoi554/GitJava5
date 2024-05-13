f9b759650ca72dc15adbb0d2ddea8395e0f96de9
Khoi05 <nguynkhoi2004@gmail.com>
2024-05-13 19:34:58 +0700

    Lab2_NguyenMInhKhoi_commi

---
+++ src/main/java/com/demo/poly01/entity/Student.java
@@ -0,0 +1,21 @@
+package com.demo.poly01.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private String id;
+    private String name;
+    private String classes;
+    private int age;
+    private String address;
+    private String hometown;
+
+
+}
